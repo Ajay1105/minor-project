70c209250dbf2fd3aaec3cc972298fd56a673c91
Ajay <aky11052003@gmail.com>
2024-04-13 14:43:20 +0530

    minor theme changes

--- lib/admin.ts
+++ lib/admin.ts
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 
 export const getIsAdmin = () => {
   const { userId } = auth();
+  //console.log(userId); // uncomment this line to see the userId in the console
   const adminIds = process.env.CLERK_ADMIN_IDS.split(", "); // stored in .env.local file as string separated by comma(,) and space( )
 
   if (!userId) return false;
