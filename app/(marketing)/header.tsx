7c2dc9a7458ea1dc2de86a1a078a1fe196ae7abe
Ajay <aky11052003@gmail.com>
2024-04-16 17:40:14 +0530

    Refactor header component and marketing page layout

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import {
   ClerkLoaded,
   ClerkLoading,
@@ -6,6 +5,7 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
+  // auth,
 } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
@@ -13,31 +13,24 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { links } from "@/config";
-import { useAuth } from "@clerk/clerk-react";
-import { useDarkModeStore } from "@/store/darkModeStore";
 
 export const Header = () => {
-  const { userId } = useAuth();
-  const { darkMode,toggleDarkMode } = useDarkModeStore();
-
-
+  // const { userId } = auth();
 
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
-        <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-          <Image
-            src="/abc.png"
-            alt="Mascot"
-            height={40}
-            width={40}
-            className="object-cover"
-          />
+      <div className="mx-auto w-full flex h-full items-center justify-between px-8">
+        <Link href="/" className="flex items-center pb-7 pt-8">
+          <div className="h-[60px] w-[60px]">
+            <img src="/abc-removebg.png" alt="Mascot" className="object-cover"/>
+          </div>
 
-          <h1 className="text-2xl font-extrabold tracking-wide">SpeechSavvy</h1>
+          <h1 className="text-2xl font-extrabold tracking-wide">
+            SpeechSavvy
+          </h1>
         </Link>
 
-        <div className="flex gap-x-3">
+        <div className="flex items-center gap-x-5">
           <ClerkLoading>
             <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
           </ClerkLoading>
@@ -62,13 +55,13 @@ export const Header = () => {
               href={links.sourceCode}
               target="_blank"
               rel="noreferrer noopener"
-              className={userId ? "pt-1.5" : "pt-3"}
+              // className={userId ? "pt-1.5" : "pt-3"}
             >
               <Image
                 src="/github.svg"
                 alt="Source Code"
-                height={20}
-                width={20}
+                height={30}
+                width={30}
               />
             </Link>
           </ClerkLoaded>
